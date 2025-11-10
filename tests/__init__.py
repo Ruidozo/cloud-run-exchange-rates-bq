91b7ba57ceba82f00678550afe7515a53bd8bfb7
Ruidozo <128640010+Ruidozo@users.noreply.github.com>
2025-11-10 15:25:19 +0000

    feat: add __init__.py to tests package for module imports

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# Import test modules to make them available as package imports
+from tests import test_bq, test_converter, test_edge_cases, validation_test
+
+__all__ = [
+    'test_converter',
+    'test_edge_cases', 
+    'test_bq',
+    'validation_test',
+]
